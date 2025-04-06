9b38e29aa3432356e5bff1407a9a207af376b1d1
norbiros <norbiros@protonmail.com>
2025-04-06 12:57:38 +0200

    feat: Very basic implementation

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@rsbuild/core';
 export default defineConfig({
   html: {
     template: 'src/index.html',
-    title: 'Ranking Odpowiedzialności Społecznej'
+    title: 'Ranking Odpowiedzialności Społecznej',
   },
   server: {
     publicDir: {
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import './index.css';
+
+import Alpine from 'alpinejs';
+import PineconeRouter from 'pinecone-router';
+// @ts-ignore
+import AutoAnimate from '@marcreichel/alpine-auto-animate';
+
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+
+const supabase: SupabaseClient = createClient(
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
+);
+window.supabase = supabase;
+
+window.Alpine = Alpine;
+
+Alpine.plugin(PineconeRouter);
+Alpine.plugin(AutoAnimate);
+
+Alpine.start();
