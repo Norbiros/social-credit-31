87d18adb4f98aebb468458dbde2cdb6520a11686
norbiros <norbiros@protonmail.com>
2025-04-06 13:05:45 +0200

    refactor: Reformat code

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import PineconeRouter from 'pinecone-router';
 // @ts-ignore
 import AutoAnimate from '@marcreichel/alpine-auto-animate';
 
-import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { createClient, type SupabaseClient } from '@supabase/supabase-js';
 
 const supabase: SupabaseClient = createClient(
   import.meta.env.PUBLIC_SUPABASE_URL,
